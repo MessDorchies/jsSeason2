447b3f7246b06a8e5df689ac161278ace65d4b23
Mess Dorchies <messaouddorchies@gmail.com>
2021-10-06 10:15:24 +0200

    Commit1

---
+++ Ajax/assets/exoAjax.js
@@ -0,0 +1,17 @@
+async function getQuote(){
+
+    let answer = await fetch( "https://thatsthespir.it/api");
+    let quote = answer.json();
+    return quote;
+}
+
+let quoteContent = document.getElementById("quote");
+let authorContent = document.getElementById("author");
+let imgContainer = document.getElementById("imgContainer");
+getQuote().catch(async error => console.error(error)).then(quote => {
+    quoteContent.innerHTML = JSON.stringify(quote.quote);
+    authorContent.innerHTML = JSON.stringify(quote.author);
+    imgContainer.innerHTML = '<img src =" ' + quote.photo + '">'
+    
+});
+console.log(getQuote());
